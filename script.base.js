1534e9fc816c1365349815bd599384d347c6fabe
iPAGroove <ipagroove@gmail.com>
2025-07-05 19:29:39 +0300

    Create script.base.js

---
+++ script.base.js
@@ -0,0 +1,16 @@
+// Анимация звезд
+const stars = document.querySelector('.stars');
+if (stars) {
+  const numStars = 130;
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement('div');
+    star.style.position = 'absolute';
+    star.style.top = Math.random() * 100 + '%';
+    star.style.left = Math.random() * 100 + '%';
+    star.style.width = star.style.height = (Math.random() * 2.5 + 1) + 'px';
+    star.style.background = '#fff';
+    star.style.opacity = Math.random() * 0.6 + 0.2;
+    star.style.borderRadius = '50%';
+    stars.appendChild(star);
+  }
+}
